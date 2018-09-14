74616ecd3bc3bb920bdedab7160c982187f6a4d7
Norberto Sanchez Martinez <norber145@gmail.com>
2018-09-14 13:34:35 +0200

    chapter 3

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -35,5 +35,23 @@ insertEntry = Cons
 findEntry :: String -> String -> AddressBook -> Maybe Entry
 findEntry firstName lastName = head <<< filter filterEntry
   where
-  filterEntry :: Entry -> Boolean
-  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+-- 2.
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street book = head $ filter filterEntry book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.address.street == street
+
+-- 3.
+entryExists :: String -> String -> AddressBook -> Boolean
+entryExists firstName lastName book = not $ isNothing $ findEntry firstName lastName book
+
+-- 4.
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy sameName
+  where
+    sameName :: Entry -> Entry -> Boolean
+    sameName e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
