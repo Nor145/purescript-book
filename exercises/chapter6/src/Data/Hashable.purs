bb01578248f3442e5ec449d7c7e7e7b9f00a6da3
Norberto Sanchez Martinez <norber145@gmail.com>
2018-09-23 23:11:01 +0200

    chapter 6

--- exercises/chapter6/src/Data/Hashable.purs
+++ exercises/chapter6/src/Data/Hashable.purs
@@ -62,3 +62,82 @@ instance hashTuple :: (Hashable a, Hashable b) => Hashable (Tuple a b) where
 instance hashEither :: (Hashable a, Hashable b) => Hashable (Either a b) where
   hash (Left a) = hashCode 0 `combineHashes` hash a
   hash (Right b) = hashCode 1 `combineHashes` hash b
+
+-- 6.4
+
+-- 1.
+newtype Complex = Complex
+  { real :: Number
+  , imaginary :: Number
+  }
+
+instance showComplex :: Show Complex where
+  show (Complex x) = "(" <> show x.real <> " + " <> show x.imaginary <> "i)"
+
+instance eqComplex :: Eq Complex where
+  eq (Complex x) (Complex y) = x.real == y.real && x.imaginary == y.imaginary
+
+-- 6.7
+
+-- 1.
+data NonEmpty a = NonEmpty a (Array a)
+
+instance eqNonEmpty :: (Eq a) => Eq (NonEmpty a) where
+  eq (NonEmpty x xs) (NonEmpty y ys) = x == y && xs == ys
+
+-- 2.
+instance semigroupNonEmpty :: Semigroup (NonEmpty a) where
+  append (NonEmpty x xs) (NonEmpty y ys) = NonEmpty x (xs <> [y] <> ys)
+
+-- 3.
+instance functorNonEmpty :: Functor NonEmpty where
+  map f (NonEmpty x xs) = NonEmpty (f x) (map f xs)
+
+-- 4.
+data Extended a = Finite a | Infinite
+
+instance eqExtended :: (Eq a) => Eq (Extended a) where
+  eq Infinite Infinite = true
+  eq (Finite x) (Finite y) = x == y
+  eq _ _ = false
+
+instance ordExtended ::  (Ord a) => Ord (Extended a) where
+  compare Infinite Infinite = EQ
+  compare Infinite _ = GT
+  compare _ Infinite = LT
+  compare (Finite x) (Finite y) = compare x y
+
+-- 5.
+instance foldableNonEmpty :: Foldable NonEmpty where
+  foldl f acc (NonEmpty x xs) = foldl f acc ([x] <> xs)
+  foldr f acc (NonEmpty x xs) = foldr f acc ([x] <> xs)
+  foldMap f (NonEmpty x xs) = foldMap f ([x] <> xs)
+
+-- 6.
+data OneMore f a = OneMore a (f a)
+
+instance foldableOneMore :: Foldable f => Foldable (OneMore f) where
+  foldr f acc (OneMore x xs) = f x (foldr f acc xs)
+  foldl f acc (OneMore x xs) = foldl f (f acc x) xs
+  foldMap f (OneMore x xs) = append (f x) (foldMap f xs)
+
+-- 6.11
+
+class Monoid m <= Action m a where
+    act :: m -> a -> a
+
+-- 3
+instance arrayAction :: Action m a => Action m (Array a) where
+  act m xs = map (act m) xs
+
+-- 4
+newtype Self m = Self m
+
+instance selfAppend :: Action m a => Action m (Self m) where
+  act _ (Self x) = Self (x <> x)
+
+-- 6.12
+
+2.
+hashDuplicates :: forall a. (Hashable a) => Array a -> Boolean
+hashDuplicates a = length a /= length (nubBy hashEqual a)
